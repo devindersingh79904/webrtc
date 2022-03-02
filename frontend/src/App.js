509f27e50b66c3ef294e5f530464aed880d3e14b
devinder singh <devindersingh79904@gmail.com>
2022-03-02 15:46:01 +0530

    react routing implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,13 @@
 import { useEffect } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
 import { connectWithWebSocket } from './utils/wssConnection';
 
 function App() {
@@ -9,9 +17,12 @@ function App() {
   },[])
 
   return (
-    <div className="App">
-      <h1>I am running from react app</h1>
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+    </Router>
   );
 }
 
